30178733df0df208afc202e3f2b14a9417ae4c2b
pedrohma95 <pedrohma95@gmail.com>
2020-01-19 19:32:02 -0800

    removing warnings

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -2,41 +2,32 @@
 
 
 import React, { useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
+import { ListGroup } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import TrackService from '../api/track.service';
 
 const Tracks = () => {
 
-  const { id } = useSelector(
+  const id = useSelector(
     (state) => state.loggedInUser
   );
 
   const [tracks, setTracks] = useState(null);
 
   useEffect(() => {
-    TrackService.getTracks({ id }).then(tracks => {
+    TrackService.getTracks(id).then(tracks => {
       setTracks(tracks);
     });
 
-  }, []);
+  }, [id]);
 
   return (
-    <div>
-        {tracks && tracks.map(track => {
-          return <Card>
-            <Card.Body>
-              <Card.Title>{track.title}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">{track.bpm}</Card.Subtitle>
-              <Card.Text>
-                {track.uuid} - created at {track.created_at}.
-              </Card.Text>
-              <Card.Link href="#">{track.title}</Card.Link>
-            </Card.Body>
-
-          </Card>
-        })}
-    </div>
+    <ListGroup>
+      {tracks !== null && tracks.map((track, index) => {
+        return <ListGroup.Item
+          key={index}>{track.uuid} - <small>created at {track.created_at}</small></ListGroup.Item>
+      })}
+    </ListGroup>
   );
 }
 
